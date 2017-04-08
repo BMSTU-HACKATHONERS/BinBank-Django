ada0e337cedd1aeae8f2bf5c43d3255df7af9d09
leoniknik <leoniknik@mail.ru>
2017-04-08 15:24:39 +0300

    fixes

--- BinBank_Django/BinBank/models.py
+++ BinBank_Django/BinBank/models.py
@@ -1,57 +1,64 @@
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import BaseUserManager
-from django.contrib.auth.base_user import AbstractBaseUser
+
+
+# from django.contrib.auth.base_user import AbstractBaseUser
 
 
 class Parent(models.Model):
-    card_ID=models.CharField(verbose_name='card_id',max_length=255,default="")
-    first_name=models.CharField(verbose_name='first_mane',max_length=255,default="")
-    last_name=models.CharField(verbose_name='last_name',max_length=255,default="")
-    phone=models.CharField(verbose_name='phone',max_length=255,default="")
-    email=models.CharField(verbose_name='email',max_length=255,default="")
-    balance=models.IntegerField(verbose_name='card_id',default=0)
+    card_ID = models.CharField(verbose_name='card_id', max_length=255, default="")
+    first_name = models.CharField(verbose_name='first_mane', max_length=255, default="")
+    last_name = models.CharField(verbose_name='last_name', max_length=255, default="")
+    phone = models.CharField(verbose_name='phone', max_length=255, default="")
+    email = models.CharField(verbose_name='email', max_length=255, default="")
+    balance = models.IntegerField(verbose_name='card_id', default=0)
     password = models.CharField(verbose_name='password', max_length=255, default="")
 
 
 class Child(models.Model):
-    #card_ID = models.CharField(verbose_name='card_id', max_length=255, default="")
+    # card_ID = models.CharField(verbose_name='card_id', max_length=255, default="")
     first_name = models.CharField(verbose_name='first_mane', max_length=255, default="")
     last_name = models.CharField(verbose_name='last_name', max_length=255, default="")
     phone = models.CharField(verbose_name='phone', max_length=255, default="")
     email = models.CharField(verbose_name='email', max_length=255, default="")
     balance = models.IntegerField(verbose_name='balance', default=0)
-    parent_ID = models.ForeignKey(Parent,null=True)
-    password=models.CharField(verbose_name='password', max_length=255, default="")
-    #ограничения
-    #биты
-    online_cashing_bit=models.BooleanField(verbose_name='online_cashing_bit', default=False, db_index=True)
-    #цифры
-    max_per_day=models.IntegerField(verbose_name='max_per_day', default=0)
-    max_payment=models.IntegerField(verbose_name='max_payment', default=0)
-    low_balance=models.IntegerField(verbose_name='low_balance', default=0)
-    max_cashing=models.IntegerField(verbose_name='max_cashing', default=0)
+    parent = models.ForeignKey(Parent, null=True)
+    password = models.CharField(verbose_name='password', max_length=255, default="")
+    # ограничения
+    # биты
+    online_cashing_bit = models.BooleanField(verbose_name='online_cashing_bit', default=False, db_index=True)
+    # цифры
+    max_per_day = models.IntegerField(verbose_name='max_per_day', default=0)
+    max_payment = models.IntegerField(verbose_name='max_payment', default=0)
+    low_balance = models.IntegerField(verbose_name='low_balance', default=0)
+    max_cashing = models.IntegerField(verbose_name='max_cashing', default=0)
+
 
 class Place(models.Model):
-    child_ID=models.ForeignKey(Child,null=True)
-    place_name=models.CharField(verbose_name='place_name', max_length=255, default="")
-    longitude=models.FloatField(verbose_name='longtide',default=0)
+    child = models.ForeignKey(Child, null=True)
+    place_name = models.CharField(verbose_name='place_name', max_length=255, default="")
+    longitude = models.FloatField(verbose_name='longtide', default=0)
     latitude = models.FloatField(verbose_name='latitide', default=0)
-    enable_bit=models.BooleanField(verbose_name='enable_bit',default=True)
+    enable_bit = models.BooleanField(verbose_name='enable_bit', default=True)
+
 
 class Income(models.Model):
-    child_ID=models.ForeignKey(Child,null=True)
-    parent_ID=models.ForeignKey(Parent,null=True)
-    sum=models.IntegerField(verbose_name='sum', default=0)
-    type=models.IntegerField(verbose_name='type', default=0)
+    child = models.ForeignKey(Child, null=True)
+    parent = models.ForeignKey(Parent, null=True)
+    sum = models.IntegerField(verbose_name='sum', default=0)
+    type = models.IntegerField(verbose_name='type', default=0)
+
 
 class Expense(models.Model):
-    child_ID = models.ForeignKey(Child, null=True)
-    sum=models.IntegerField(verbose_name='sum', default=0)
-    type=models.IntegerField(verbose_name='type', default=0)
+    child = models.ForeignKey(Child, null=True)
+    sum = models.IntegerField(verbose_name='sum', default=0)
+    type = models.IntegerField(verbose_name='type', default=0)
+
 
 class IncomeType(models.Model):
-    type=models.CharField(verbose_name='type', max_length=255, default="")
+    type = models.CharField(verbose_name='type', max_length=255, default="")
+
 
 class ExpenseType(models.Model):
-    type=models.CharField(verbose_name='type', max_length=255, default="")
+    type = models.CharField(verbose_name='type', max_length=255, default="")
--- BinBank_Django/BinBank/views.py
+++ BinBank_Django/BinBank/views.py
@@ -1,47 +1,56 @@
-from BinBank.models import Parent,Child,Place,Expense,Income
+from BinBank.models import Parent, Child, Place, Expense, Income
 from django.http import JsonResponse
 
 
 def signin(request):
     try:
         email = request.POST['email']
+        print(email)
         password = request.POST['password']
-        eror=False
-        child=None
-        parent=None
+        print(password)
+        eror = False
+        child = None
+        parent = None
         try:
-            child=Child.get(email=email,password=password)
-            parent=Parent.get(id=child.parerent_ID)
+            child = Child.objects.get(email=email, password=password)
+            print(child.email)
+            parent = Parent.objects.get(child=child)
         except Exception:
             return JsonResponse({"code": 1})
         else:
-            return JsonResponse({"code": 0, "data": {
-                                                        "parent":{
-                                                                    "parent_id":parent.id,
-                                                                    "first_name":parent.first_name,
-                                                                    "last_name":parent.last_name,
-                                                                    "phone":parent.phone,
-                                                                    "email":parent.email,
-                                                                    "balance":parent.balance,
-                                                                    "password":parent.password,
-                                                                },
-                                                    "child":{
-                                                        "child_id":child.id,
-                                                        "first_name":child.first_name,
-                                                        "last_name":child.last_name,
-                                                        "phone":child.phone,
-                                                        "email":child.email,
-                                                        "balance":child.balance,
-                                                        "parerent_id":child.parerent_ID,
-                                                        "password":child.password,
-                                                        "online_cashing_bit":child.online_cashing_bit,
-                                                        "max_per_day":child.max_per_day,
-                                                        "max_payment":child.max_payment,
-                                                        "low_balance":child.low_balance,
-                                                        "max_cashing":child.max_cashing,
-                                                    }
+            c_bit=0
+            if(child.online_cashing_bit):
+                c_bit=1
+            else:
+                c_bit=0
 
-                                                }})
+            return JsonResponse({"code": 0, "data": {
+                "parent": {
+                    "parent_id": parent.id,
+                    "first_name": parent.first_name,
+                    "last_name": parent.last_name,
+                    "phone": parent.phone,
+                    "email": parent.email,
+                    "balance": parent.balance,
+                    "password": parent.password,
+                },
+                "child": {
+                    "child_id": child.id,
+                    "first_name": child.first_name,
+                    "last_name": child.last_name,
+                    "phone": child.phone,
+                    "email": child.email,
+                    "balance": child.balance,
+                    "parent_id": child.parent.id,
+                    "password": child.password,
+                    "online_cashing_bit": c_bit,
+                    "max_per_day": child.max_per_day,
+                    "max_payment": child.max_payment,
+                    "low_balance": child.low_balance,
+                    "max_cashing": child.max_cashing,
+                }
+
+            }})
     except Exception as e:
         print(e)
         return JsonResponse({"code": 1})
@@ -53,13 +62,13 @@ def request_balance(request):
         balance = request.POST['balance']
 
         try:
-            child = Child.get(id=child_ID)
-            parent = Parent.get(id=child.parerent_ID)
-            child.balance+=int(balance)
-            parent.balance-=int(balance)
+            child = Child.objects.get(id=child_ID)
+            parent = Parent.objects.get(id=child.parerent_ID)
+            child.balance += int(balance)
+            parent.balance -= int(balance)
             child.save()
             parent.save()
-            Income.objects.create(type=1,sum=balance,child_ID=child_ID,parent_ID=parent.id)
+            Income.objects.create(type=1, sum=balance, child_ID=child_ID, parent_ID=parent.id)
         except Exception:
             return JsonResponse({"code": 1})
         else:
@@ -73,7 +82,7 @@ def get_expense(request):
     try:
         child_ID = request.POST['child_id']
         try:
-            expenses=Expense.objects.filter(child_ID=child_ID)
+            expenses = Expense.objects.filter(child_ID=child_ID)
             print(expenses)
             data = list(expenses)
         except Exception:
@@ -89,7 +98,7 @@ def get_income(request):
     try:
         child_ID = request.POST['child_id']
         try:
-            incomes=Income.objects.filter(child_ID=child_ID)
+            incomes = Income.objects.filter(child_ID=child_ID)
             print(incomes)
             data = list(incomes)
         except Exception:
@@ -99,7 +108,6 @@ def get_income(request):
     except Exception as e:
         print(e)
         return JsonResponse({"code": 1})
-
 
 
 def sos(request):
@@ -121,12 +129,13 @@ def sos(request):
         return JsonResponse({"code": 1})
 
 
-def operation(child,sum,cashing):
-    ask_parent=False
-    if(child.balance-sum<child.low_balance):ask_parent=True
-    if(sum>child.max_payment):ask_parent=True
-    if(cashing and sum>child.max_cashing): ask_parent=True
-    Expense.objects.create(type=1,sum=sum,child_ID=child.id)
+def operation(child, sum, cashing):
+    ask_parent = False
+    if (child.balance - sum < child.low_balance): ask_parent = True
+    if (sum > child.max_payment): ask_parent = True
+    if (cashing and sum > child.max_cashing): ask_parent = True
+    Expense.objects.create(type=1, sum=sum, child_ID=child.id)
+
 
 def online_payment(request):
     try:
@@ -135,14 +144,10 @@ def online_payment(request):
         try:
             child = Child.get(id=child_ID)
             parent = Parent.get(id=child.parerent_ID)
-            if(child.online_cashing_bit==1):
-                operation(child,balance,False)
+            if (child.online_cashing_bit == 1):
+                operation(child, balance, False)
             else:
                 return JsonResponse({"code": 1})
-
-
-
-
         except Exception:
             return JsonResponse({"code": 1})
         else:
@@ -167,6 +172,7 @@ def payment(request):
         print(e)
         return JsonResponse({"code": 1})
 
+
 def cashing(request):
     try:
         child_ID = request.POST['child_id']
@@ -181,5 +187,3 @@ def cashing(request):
     except Exception as e:
         print(e)
         return JsonResponse({"code": 1})
-
-
